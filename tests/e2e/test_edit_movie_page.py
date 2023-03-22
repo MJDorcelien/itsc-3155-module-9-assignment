cb08777837967a023ee96d11e1ca3b049f83bd3f
Devon <devonlemasters4@gmail.com>
2023-03-22 18:08:32 -0400

    Implementing edit movies feature

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -1,1 +1,37 @@
 # TODO: Feature 5
+from flask.testing import FlaskClient
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+movie_repository = get_movie_repository();
+movie_repository.clear_db()
+movie = movie_repository.create_movie("Midsommar", "Ari Aster", 9)
+
+def test_edit_movie(test_app: FlaskClient):  
+    response = test_app.get(f'/movies/{movie.movie_id}/edit')
+    assert response.status_code == 200
+    
+    assert b'<h2 class="mb-3">Edit movie information below:</h2>' in response.data
+    assert b'<label for="title">Title:</label>' in response.data
+
+    response2 = test_app.post(f'/movies/{movie.movie_id}', data={
+        'title' : 'Midsommar',
+        'director' : 'Ari Aster',
+        'rating' : 5 ,
+    }, follow_redirects=True)
+
+    assert response2.status_code == 200
+    data = response2.data.decode('utf-8')
+
+    assert '<h1 class="pb-2">Title: Midsommar</h4>' in data
+    assert '<h4 class="pb-2">Director: Ari Aster</h4>' in data
+    assert '<h4 class="pb-2">Rating: 5</h4>' in data
+
+    
+
+
+
+
+
+
+
--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,1 +1,26 @@
 # TODO: Feature 5
+from src.repositories.movie_repository import get_movie_repository 
+from src.models.movie import Movie
+from app import update_movie
+
+movie_repository = get_movie_repository()
+
+
+def test_update_movie():
+    movie_repository.clear_db()
+    movie = movie_repository.create_movie("Cocaine Bear", "Elizabeth Banks", 3)
+    movie2 = movie_repository.create_movie("Devil Wears Prada", "David Frankel", 4)
+    id = movie.movie_id
+    id2 = movie2.movie_id
+
+    up_movie = movie_repository.update_movie(id, "Cocaine Bear", "Elizabeth Banks", 2)
+    up_movie2 = movie_repository.update_movie(id2, "The Devil Wears Prada", "David Frankel", 5)
+    
+    assert up_movie.rating == 2
+    assert up_movie.title == movie.title
+    assert up_movie.director == movie.director
+
+    assert up_movie2.title != "Devil Wears Prada"
+    assert up_movie2.director == movie2.director
+    assert up_movie2.rating == 5
+
